b7ea2590a343fbd26fcf206ce059450ebf728891
vistaing <632592761@qq.com>
2015-06-04 15:36:42 +0800

    Update index

--- js/handleJson.js
+++ js/handleJson.js
@@ -28,9 +28,30 @@ $(function() {
             init=0,
             linkHTML='';
 
+        function linkName(link) {
+            switch (link) {
+                case 'iqiyi':
+                    return '爱奇艺';
+                case 'bili':
+                    return 'Bilibili';
+                case 'letv':
+                    return '乐视';
+                case 'matong':
+                    return '芒果TV';
+                case 'pptv':
+                    return 'PPTV';
+                case 'tx':
+                    return '腾讯';
+                case 'tudou':
+                    return '土豆';
+                case 'youku':
+                    return '优酷';
+            }
+        }
+        
         for(var link in obj.linklist){
         	linkHTML+='<a href="'+obj.linklist[link]+'" target="_blank">';
-        	linkHTML+='<span class="badge badge-'+link+'">'+link+'</span>';
+        	linkHTML+='<span class="badge badge-'+link+'">'+linkName(link)+'</span>';
         	linkHTML+='</a>';
         	if(init<linkListNum-1){
         		linkHTML+='&nbsp;<span class="divide">|</span> &nbsp;';
@@ -40,7 +61,7 @@ $(function() {
         	init++;
         }
         $target.append('<tr class="uk-table-middle">'
-			+'<td class="uk-width-3-6" id="bgm0101"><a href="#0101" data-uk-modal>'+obj.titleCHN+'<br><i class="uk-text-muted">'+obj.titleJPN+'</i></a></td>'
+			+'<td class="uk-width-3-6" id="'+obj.bgmID+'"><a href="'+obj.bgmInfoID+'" data-uk-modal>'+obj.titleCHN+'<br><i class="uk-text-muted">'+obj.titleJPN+'</i></a></td>'
 			+'<td class="uk-width-1-6">'+obj.episodeThisWeek+' <span class="uk-text-muted">/ '+obj.episodeTotal+'</span></td>'
 			+'<td class="uk-width-2-6">'
 				+linkHTML
